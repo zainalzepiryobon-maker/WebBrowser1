31ddbba7f057a9aa5f8c40ab2b4f9ed89a37bc3b
root <root@localhost>
2025-11-22 07:12:22 +0800

    auto publish 1763766741877

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 document.getElementById("button2").textContent = `click me too`;
 
+let y;
 let x;
-x = 100;
+
+x = 26;
+y = x ** 7;
+y = (y / (26 ** 7) + 27) % 3
 
 console.log(`the value of x is ${x}`)
 
+console.log(`the value of y is ${y}`)
