4b4d0b50c13236e81c88d2490b0aef4194d4eff8
root <root@localhost>
2025-11-22 07:28:43 +0800

    auto publish 1763767723343

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ y = (y / (26 ** 7) + 27) % 3
 console.log(`the value of x is ${x}`)
 
 console.log(`the value of y is ${y}`)
+
+// username = window.prompt(`What Is Your Username?`)
+
+let username;
+
+document.getElementById("Submit").onclick = function(){
+  username = document.getElementById("Text").value;
+  document.getElementById("H11").textContent = `Hello ${username}`
+  console.log(username);
+}
+
