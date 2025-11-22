b701c23c6c2ee3b4a632a5e27578925023159581
root <root@localhost>
2025-11-22 08:27:23 +0800

    auto publish 1763771242952

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.getElementById("button2").textContent = `click me too`;
 
 let y;
 let x;
-let x;
+let z;
 
 x = 26;
 y = x ** 7;
@@ -14,8 +14,8 @@ console.log(`the value of y is ${y}`)
 
 let username;
 
-document.getElementById("Submit").onclick = function(){
-  username = document.getElementById("Text").value;
+document.getElementById("Submit1").onclick = function(){
+  username = document.getElementById("Text1").value;
   document.getElementById("H11").textContent = `Hello ${username}`
   console.log(username);
 }
@@ -25,4 +25,41 @@ document.getElementById("Submit").onclick = function(){
 // age += 1;
 
 // console.log(age, typeof age);
+
+const PI = 3.14159
+let radius
+let circumference
+
+document.getElementById("Submit2").onclick = function(){
+  radius = document.getElementById("Text2").value;
+  radius = Number(radius);
+  circumference = 2 * PI * radius;
+  document.getElementById("H12").textContent = `The circumference of the circle is ${circumference}cm`
+  console.log(username);
+}
+
+// BORDER
+
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+const increase = document.getElementById("increase");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increase.onclick = function(){
+  count++;
+  countLabel.textContent = count;
+}
+
+decrease.onclick = function(){
+  count--;
+  countLabel.textContent = count;
+}
+
+reset.onclick = function(){
+  count = 0;
+  countLabel.textContent = count;
+}
+
+//BORDER
 
